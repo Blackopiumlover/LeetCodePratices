d287299eb171c35605752700dade56346857bdd2
jinyongjia <jinyongjia@xiaomi.com>
2022-02-18 14:25:16 +0800

    Adding LeetCode 2169 count operations to territory.
    Signed-off-by: jinyongjia <jinyongjia@xiaomi.com>

---
+++ src/LeetCode2169CountOperations.java
@@ -0,0 +1,17 @@
+public class LeetCode2169CountOperations {
+    public static void main(String[] args) {
+        System.out.println(countOperations(2,3));
+    }
+    public static int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 > 0 && num2 > 0) {
+            if (num1 >= num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+            count += 1;
+        }
+        return count;
+    }
+}
